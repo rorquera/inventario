531378adc13e50d1c164b6b69696a7b559c60af0
rorquera <arckanhell1@gmail.com>
2024-12-15 11:05:11 -0500

    mosdificacion en consulta con joins

--- scripts/consultas_pronosticos.sql
+++ scripts/consultas_pronosticos.sql
@@ -4,9 +4,13 @@ FROM public.t_partido
 WHERE DATE(fecha) = '2024-06-22';
 
 --pronostico de un usuario especifico
-SELECT *
-FROM public.t_pronostico
-WHERE usuario='1725456895'
+SELECT p.id, u.nombre, u.cedula, p.partido, eqa.nombre as nombreqa, eqb.nombre as nombreqb, p.marcador_eq_a, p.marcador_eq_b
+FROM public.t_pronostico p
+join public.t_usuario u on p.usuario=u.cedula
+join public.t_partido pa on pa.id = p.partido
+join public.t_equipo eqa on eqa.codigo=p.equipo_a
+join public.t_equipo eqb on eqb.codigo=p.equipo_b
+WHERE p.usuario='1725456895';
 
 --pronostico de un partido especifico
 SELECT *
